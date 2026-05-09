2134d3ab045c66e0180ceee9676bc96c5e8d4fad
Rishu Singla <rishusingla289@gmail.com>
2026-05-09 13:16:07 +0530

    "backend testing"

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,9 @@ connectDB();
 
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
